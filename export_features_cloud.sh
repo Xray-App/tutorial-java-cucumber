cd7110666e6082ccb612f01f7fa9fe4648365a0e
Sergio Freire <sergio.freire@gmail.com>
2022-02-01 10:26:54 +0000

    Update export_features_cloud.sh

--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -2,7 +2,7 @@
 
 
 
-BASE_URL=https://xray.cloud.xpand-it.com
+BASE_URL=https://xray.cloud.getxray.app
 KEYS="CALC-640;CALC-641"
 KEYS="XT-17;XT-18"
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
