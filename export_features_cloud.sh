16c552914ca9f062e524876ba001c9d97b878d23
Sergio Freire <sergio.freire@gmail.com>
2024-12-16 11:29:42 +0000

    update cloud endpoints

--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -5,8 +5,8 @@
 BASE_URL=https://xray.cloud.getxray.app
 KEYS="CALC-640;CALC-641"
 KEYS="XT-17;XT-18"
-token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
-curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v1/export/cucumber?keys=$KEYS" -o features.zip
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v2/authenticate| tr -d '"')
+curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v2/export/cucumber?keys=$KEYS" -o features.zip
 
 rm -rf features/*.feature
 unzip -o features.zip -d features
--- import_features_cloud.sh
+++ import_features_cloud.sh
@@ -5,5 +5,5 @@ PROJECT=CALC
 rm -f features.zip
 zip -r features.zip src/test/resources/calculator/ -i \*.feature
  
-token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
-curl -H "Content-Type: multipart/form-data" -H "Authorization: Bearer $token"  -F "file=@features.zip" "$BASE_URL/api/v1/import/feature?projectKey=$PROJECT"
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v2/authenticate"| tr -d '"')
+curl -H "Content-Type: multipart/form-data" -H "Authorization: Bearer $token"  -F "file=@features.zip" "$BASE_URL/api/v2/import/feature?projectKey=$PROJECT"
